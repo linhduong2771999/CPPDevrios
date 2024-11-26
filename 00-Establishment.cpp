ac98e067e1c39227908677479d78baf2a41f69ea
unknown <linhduong2771999@gmail.com>
2024-11-26 13:09:38 +0700

    all files

---
+++ 00-Establishment.cpp
@@ -0,0 +1,17 @@
+/* INTRODUCTION
+    ⁂⁜※⁑⁇⁈
+    
+    ⁈ Preface
+    ⁂ On August 26, 2024, the Task Control or Center of Operation decided to start a new C++ project replacing 
+    the former one, which named "Hoc hanh nghiem tuc" was created on September 25, 2022 (according to the "New Laptop" 
+    date). The new official name of the project is "Devrios" (meaning Development + Kyrios).
+
+    ⁇ Why to create
+    ⁂ Devrios aims to establish an impeccable and sustainable curriculum for learning and mastering C++ programming 
+    language for beginners without any interruptions. In the past, many programmers detected a lot of mistakes and 
+    errors from the previous project that had been crucial causes for the delay of learning c++, both internal and 
+    external causes. With a clarity and firmness of mind from predecessors, they build and construct a whole new system 
+    of learning c++ that deals with all its previous negative points for beginners easy to follow. 
+    Devrios fixs and repairs all the system encompassing the name, structure, style of leaning, and exercise.    
+
+*/
---
+++ Subjects/01. Arrays & Vectors/arrayVector.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+
+/*
+    If you use the [] to access vector's element. Vector will not do bound checking. 
+
+    --------
+    - If you type texts into integer: cin >> number || input: abc
+    - "abc" will be put into the "input buffer" [a,b,c]
+    - "cin" reads the texts from the buffer   
+    - Since the input is a texts "cin" cannot parse its into an integer variable.
+    - "cin" enters a fail stage, the input buffer will not be consumes and the 
+    -remain texts will stay in the buffer. 
+    - "cin" will skip any next cin function unless it clears the buffer by "cin.clear() & cin.ignore()".
+    -
+    - If you type into integer: cin >> number || input: 123abc
+    - "123abc" will be put into the "input buffer" [1,2,3,a,b,c]
+    - "cin" will extract and parse 123 into the integer, but not "abc", so the buffer
+    -now still contains "abc" [a,b,c] 
+    - If the next cin is integer: it enters a fail stage
+    - If the next cin is strings: it extracts "abc" from the buffer into variable
+    --------
+
+    
+*/
+int main(){
+
+    int arr1 [5] {0,1,2,3};
+    char arr2 [2] {'s', 'a'};
+    int arr3 [] {}; // prevent garbagess
+
+    cout <<  arr3[0] << endl;
+    
+    int twoDarr [] {1, 2};
+
+    vector <int> vector1 {1, 2};
+    vector <int> vector2 (12, 0);
+
+    vector1.push_back(1);
+ 
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+/*
+    understand variables
+    compile time, compiler
+    initialize list
+    how vector works behind    
+
+*/
