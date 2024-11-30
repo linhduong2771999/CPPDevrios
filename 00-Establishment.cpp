356dde80266e6fd838fb94d768590a4942b73f98
duong <linhduong2771999@gmail.com>
2024-11-30 18:40:20 +0700

    update Establishment file

--- 00-Establishment.cpp
+++ 00-Establishment.cpp
@@ -1,17 +1,47 @@
 /* INTRODUCTION
     ⁂⁜※⁑⁇⁈
     
+                                    **********************************************
+                                    *                                            * 
+                                    *                                            *
+                                    *                      *                     *
+                                    *                     * *                    *  
+                                    *                    *   *                   *   
+                                    *             * * * *     * * * *            *  
+                                    *               *             *              *   
+                                    *                 *         *                *   
+                                    *                *   *   *   *               *   
+                                    *               *  *       *  *              *
+                                    *              **             **             *
+                                    *                                            *                   
+                                    *                                            *
+                                    **********************************************
+    
     ⁈ Preface
-    ⁂ On August 26, 2024, the Task Control or Center of Operation decided to start a new C++ project replacing 
+        ⁂ On August 26, 2024, the Task Control or Center of Operation (TCCO) decided to start a new C++ project replacing 
     the former one, which named "Hoc hanh nghiem tuc" was created on September 25, 2022 (according to the "New Laptop" 
-    date). The new official name of the project is "Devrios" (meaning Development + Kyrios).
+    date). The new official name of the project is "CPPDevrios" (meaning Development + Kyrios).
 
     ⁇ Why to create
-    ⁂ Devrios aims to establish an impeccable and sustainable curriculum for learning and mastering C++ programming 
+        ⁂ CPPDevrios aims to establish an impeccable and sustainable curriculum for learning and mastering C++ programming 
     language for beginners without any interruptions. In the past, many programmers detected a lot of mistakes and 
     errors from the previous project that had been crucial causes for the delay of learning c++, both internal and 
     external causes. With a clarity and firmness of mind from predecessors, they build and construct a whole new system 
     of learning c++ that deals with all its previous negative points for beginners easy to follow. 
-    Devrios fixs and repairs all the system encompassing the name, structure, style of leaning, and exercise.    
+    CPPDevrios fixes and repairs all the system encompassing the name, structure, style of leaning, and exercise.    
+        ※ Motivation behind: In the past, the TCCO sent predecessors for learning designing website in order to build a frontend
+    career. Initially, predecessors learned and noted many important knowledge from the internet in a very smooth way (meaning
+    they did not have encounter any difficulties), they easily absorbed the knowledge and applied it into real world project,
+    and unluckily, they were not very successful in their first projects.
+        In reality, projects such as web portfolio, accounting management, or e-commerce did not reach the anticipated results  
+    (but it also cannot be considered to be a failure, all still operate at a normal and acceptable level). This caused the TCCO
+    gradually lose faith in designing website career. In the middle of 2020, the COVID-19 pandemic broke out and caused serious 
+    economic crisis for the IT industry in general, leading to a saturation in the frontend sector in particular.
+        Having predict and foresee the situation will not be good in the future, the TCCO advised predecessors to give up their 
+    studying about website design and pursue other careers. With so many choices and options such as backend, software engineer, 
+    UX-UI design, the TCCO decided to choose the major according to the predecessors' passion, which is game development (C++).
 
 */
+// Update 1.0: August 26, 2024
+// Update 2.0: November 30, 2024
+
