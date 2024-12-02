ead2eee35403d03b26fc8ce76439f97ad7d4db78
unknown <linhduong2771999@gmail.com>
2024-12-02 18:26:11 +0700

    update question file

--- 01-Questions.cpp
+++ 01-Questions.cpp
@@ -1,29 +1,29 @@
 // Questions throughout the course that have not yet answer will be place in here (below)
-// If you see nothing below, it means there are no question at the moment
+// After 3 times update the question, we decide to keep the question for later review
 
 /*
-
+    
 
     why use initialize list
     how vector works behind    
     using namespace
 
-    definition vs declaration function
+    // definition vs declaration function
     stack vs heap
     optimization strategies
     recursive
-    user-defined
+    user-defined  
 
     --OOP
-    how compiler knows cpp file when i only include header file
-    How a compiler knows from a header file, that a source file exists somewhere?
-    Move Constructor
-    Move semantics
-    --num;
-    num--;
-    why static member class cannot be const c++
+    // how compiler knows cpp file when i only include header file
+    // How a compiler knows from a header file, that a source file exists somewhere?
+    // Move Constructor
+    // Move semantics
+    // --num;
+    // num--;
+    // why static member class cannot be const c++
     friend
 
 */
 
-// Total modification: 3
+// Total modification: 4
