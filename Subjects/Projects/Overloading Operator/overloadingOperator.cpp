7aae969e6005943466411bd009f47471abfadb00
duong <linhduong2771999@gmail.com>
2024-12-04 14:52:56 +0700

    push overloading operator project file

---
+++ Subjects/Projects/Overloading Operator/overloadingOperator.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+/*
+    - (reduce 1 lv)
+    ==
+    !=
+    < ()
+    >
+    +
+    +=
+    * (update to three lv)
+    *=
+    ++
+    --
+    >>
+    <<
+*/
+
+/*
+    champ + champ = champ level 2
+    champ lv2 + champ lv2 = champ level 3
+*/
+
+class Champion{
+    public:
+        char *name;
+        int health;
+        int level;
+        
+
+};
+
+int main(){
+
+
+    return 0;
+}
