beb7fa1a59772f4c97eadaa9c7d6e0ebf6cfffbb
linhduong2771999 <linhduong2771999@gmail.com>
2025-12-01 11:39:20 +0700

    update arrayVector.cpp

--- Subjects/01. Arrays & Vectors/arrayVector.cpp
+++ Subjects/01. Arrays & Vectors/arrayVector.cpp
@@ -4,11 +4,32 @@
 
 using namespace std;
 
+/*
+    Array & Vector
+
+    --------
+    - + C++ has three different array types that are commonly used: std::vector, 
+    -std::array (modern C++), and C-style arrays.
+    - + Use std::array for constexpr arrays, and std::vector for non-constexpr arrays.
+    --------
+
+    --------
+    - When to use std::array (modern C++) vs C-styles array
+    - + std::array is an object (same like vector) built in C++ 11
+    --------
+
+    --------
+    - + C-style arrays don’t support assignment
+    --------
+
+*/
+
 int main(){
 
     int arr1 [5] {0,1,2,3};
     char arr2 [2] {'s', 'a'};
-    int arr3 [] {}; // prevent garbages
+    int leng = 5 ;
+    int arr3 [leng] {}; // prevent garbages
 
     cout <<  arr3[0] << endl;
     
